e17c3ed9751a786726c356e3db5c6cff8c646670
Jakob Busk Sørensen <busk.soerensen@gmail.com>
2023-06-16 13:08:25 +0200

    The filter function now works (somewhat) properly.

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,19 +1,19 @@
 import './root.css'
 import CardLink from './CardLink';
 import { Cocktail } from '../types';
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 function Root() {
   const [cocktails, setCocktails] = useState<Cocktail[]>([]);
+  const [keyword, setKeyword] = useState<string>('');
   
-  const keyword = "";
-
   useEffect(() => {
-    fetchCocktails().then((allCocktails: Cocktail[]) => {
-      filterCocktails(allCocktails).then(filteredCocktails => setCocktails(filteredCocktails)); 
+    fetchCocktails().then((cocktails: Cocktail[]) => {
+      setCocktails(cocktails);
     });
   }, []);
-  
+
+  const filteredCocktails: Cocktail[] = filterCocktails(cocktails);
 
   async function fetchCocktails(): Promise<Cocktail[]> {
     return fetch('/cocktails.json')
@@ -30,7 +30,7 @@ function Root() {
       });
   }  
  
-  async function filterCocktails(cocktails: Cocktail[]): Promise<Cocktail[]> {
+  function filterCocktails(cocktails: Cocktail[]): Cocktail[] {
     if (keyword.trim() === '') {
       return cocktails; 
     }
@@ -41,7 +41,7 @@ function Root() {
       const { name, ingredients } = cocktail;
       const matchingIngredients = ingredients.filter(
         (ingredient) =>
-          ingredient.name.toLowerCase().includes(keyword.toLowerCase())
+          ingredient.name.toLowerCase() == keyword.toLowerCase()
       );
   
       if (
@@ -57,30 +57,33 @@ function Root() {
     
   return (
     <>
-      <div className='w-full invisible'>
+      <div className='w-full'>
         <div className='text-left bg-black flex items-center h-12'>
           <img src='/cocktail_icon.png' alt='Cocktail icon' className='h-full ml-4' />
           <h1 className='text-xl ml-1 font-bold inline-block align-middle'>
             <span className='text-orange-200'>Dirty</span> Drinking
           </h1>
         </div>
-
         
         <div className='m-6'>
-          <form>   
-              <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
-              <div className="relative">
-                  <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                      <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                  </div>
-                  <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for cocktail name or key ingredient..." />
-                  <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+          <label htmlFor="keyword-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+          <div className="relative">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                  <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
               </div>
-          </form>
+              <input 
+                type="search" 
+                id="keyword-search" 
+                className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                placeholder="Search for cocktail name or key ingredient..." 
+                value={keyword}
+                onChange={e => setKeyword(e.target.value)}
+              />
+          </div>
         </div>
 
         <div className='grid grid-cols-2 lg:grid-cols-3 gap-4 m-6'>
-          {cocktails
+          {filteredCocktails
             .sort((a, b) => a.name.localeCompare(b.name))
             .map((cocktail: Cocktail) => (
             <CardLink 
